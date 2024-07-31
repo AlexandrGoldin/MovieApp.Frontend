7d82e202858f1cce794b01c2a1873f55ce51bc21
Alexandr Goldin <50864552+AlexandrGoldin@users.noreply.github.com>
2024-07-31 13:46:12 +0300

    Update api.js - Deleted old comment

--- src/api/api.js
+++ src/api/api.js
@@ -31,25 +31,6 @@ export const fetchApi = (url, options = {}) => {
     });
 };
 
-// export const fetchApi = (url, options = {}) => {
-//     return new Promise((resolve, reject) => {
-//         fetch(url, options)
-//             .then(response => {
-//                 if (response.status < 400) {
-//                     return response.json();
-//                 } else {
-//                     throw response;
-//                 }
-//             })
-//             .then(data => {
-//                 resolve(data);
-//             })
-//             .catch(response => {
-//                 response.json().then(error => {
-//                     reject(error);
-//                 });
-//             });
-//     });
-// };
+
 
 
